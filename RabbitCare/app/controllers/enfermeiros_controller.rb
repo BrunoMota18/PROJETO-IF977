8cce6d0fad359bab93b1344ba0d3f8ffe856cf90
BrunoMota18 <bvgm@cin.ufpe.br>
2018-10-08 00:14:17 -0300

    Controller atualizado. Schema atualizado. View de enfermeiros adicionada.

--- RabbitCare/app/controllers/enfermeiros_controller.rb
+++ RabbitCare/app/controllers/enfermeiros_controller.rb
@@ -4,5 +4,18 @@ class EnfermeirosController < ApplicationController
 
     def create
         render plain: params[:enfermeiro].inspect
+        @enfermeiro = Enfermeiro.new(enfermeiro_params)
+        @enfermeiro.save
+        redirect_to @enfermeiro
     end
+                
+    def show
+        @enfermeiro = Enfermeiro.find(params[:id])
+    end
+
+    private 
+    def enfermeiro_params
+        params.require(:enfermeiro).permit(:nome, :cpf, :coren)
+    end
+
 end
--- RabbitCare/db/migrate/20181007184254_create_enfermeiros.rb
+++ RabbitCare/db/migrate/20181007184254_create_enfermeiros.rb
@@ -4,6 +4,7 @@ class CreateEnfermeiros < ActiveRecord::Migration[5.2]
       t.references :usuarios, foreign_key: true
       t.string :coren
       t.text :tarefas #estudar a viabilidade de criar outra tabela para este multivalorado
+      
       t.timestamps
     end
   end
