04d93afe4dbecb1af8586547c7e171e168ccdad6
BrunoMota18 <bvgm@cin.ufpe.br>
2018-11-26 17:46:38 -0200

    removendo facebook para testar erro em produção.

--- RabbitCare/config/routes.rb
+++ RabbitCare/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {registrations: 'user/registrations', omniauth_callbacks: 'callbacks'}
+  devise_for :users, :controllers => {registrations: 'user/registrations'}
   devise_scope :user do
     root to: "devise/sessions#new"
     get '/login', to: 'devise/sessions#new', as: 'login'
