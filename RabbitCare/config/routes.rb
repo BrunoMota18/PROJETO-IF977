9eb6363dccdfdc84695e4c16b4f917fee8a3d78f
BrunoMota18 <bvgm@cin.ufpe.br>
2018-11-26 18:00:19 -0200

    Revertendo retirada do omniauth _callbacks.

--- RabbitCare/config/routes.rb
+++ RabbitCare/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {registrations: 'user/registrations'}
+  devise_for :users, :controllers => {registrations: 'user/registrations', omniauth_callbacks: 'callbacks'}
   devise_scope :user do
     root to: "devise/sessions#new"
     get '/login', to: 'devise/sessions#new', as: 'login'
