080648e123782f4e4dd6cfb21e74bac5de239b15
BrunoMota18 <bvgm@cin.ufpe.br>
2018-11-25 23:04:59 -0200

    Adicionado CRUD do paciente para médicos e visibilidade para os mesmos como para enfermeiros.

--- RabbitCare/app/controllers/pacientes_controller.rb
+++ RabbitCare/app/controllers/pacientes_controller.rb
@@ -4,10 +4,42 @@ class PacientesController < ApplicationController
     end
 
     def new
+        @paciente = Paciente.new
     end
 
     def show
         @paciente = Paciente.find(params[:id])
         render 'show'
+    end
+
+    def create
+        @paciente = Paciente.new(paciente_params)
+        @paciente.medico_crm = current_user.crm
+        @paciente.save
+        redirect_to @paciente
+    end
+       
+    def destroy
+        @paciente = Paciente.find(params[:id])
+        @paciente.destroy
+        redirect_to @paciente
+    end
+
+    def edit
+        @paciente = Paciente.find(params[:id])
+    end
+
+    def update
+        @paciente = Paciente.find(params[:id])
+        if @paciente.update_attributes(paciente_params)
+            redirect_to @paciente
+        else
+            render 'edit'
+        end
+    end
+
+    private
+    def paciente_params
+        params.require(:paciente).permit(:cpf_responsavel, :nome_responsavel, :nome, :estado, :enfermeiro_coren)
     end
 end
